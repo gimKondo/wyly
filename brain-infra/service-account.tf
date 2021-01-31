06832372c99805a021bb0ef5e7417b5a3d1b1a70
Kondo Daisuke <gim0kondo@gmail.com>
2021-01-31 19:07:03 +0900

    :green_heart: create SA for local app

--- brain-infra/service-account.tf
+++ brain-infra/service-account.tf
@@ -1,3 +1,4 @@
+# cloud-run-service-account
 resource "google_service_account" "cloud-run-service-account" {
   account_id   = "cloud-run-service-account"
   display_name = "A service account for app on Cloud Run"
@@ -6,4 +7,18 @@ resource "google_service_account" "cloud-run-service-account" {
 resource "google_project_iam_member" "cloud-run-agent-iam" {
   role   = "roles/run.serviceAgent"
   member = "serviceAccount:${google_service_account.cloud-run-service-account.email}"
+}
+
+# local-app-service-account
+resource "google_service_account" "local-app-service-account" {
+  account_id   = "local-app-service-account"
+  display_name = "A service account for app on local"
+}
+resource "google_project_iam_member" "viewer-for-local-app-iam" {
+  role   = "roles/viewer"
+  member = "serviceAccount:${google_service_account.local-app-service-account.email}"
+}
+resource "google_project_iam_member" "storage-admin-for-ci-iam" {
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.sa-for-ci.email}"
 }
