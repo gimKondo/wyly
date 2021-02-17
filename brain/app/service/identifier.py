46d8de72b2dc992605fb4d87384c2e3917d1ee85
Kondo Daisuke <gim0kondo@gmail.com>
2021-02-18 04:10:51 +0900

    :sparkles: implement identify logic base

---
+++ brain/app/service/identifier.py
@@ -0,0 +1,53 @@
+from typing import Any, AnyStr, Dict, List
+
+from ..gcp.vision import label_image
+
+
+def identify(image: AnyStr) -> List[Any]:
+    label = label_image(image)
+    print(label)
+    return _build_answer(label)
+
+
+def _build_answer(label: Dict[str, Any]):
+    name = label["name"]
+    score = label["score"]
+    if name == "Plant":
+        return {
+            "answers": [
+                {
+                    "speciesName": "タチツボスミレ",
+                    "respondentName": "ミノリ",
+                    "respondentImageURL": "https://example.com/charactors/minori/image.jpg",
+                    "score": score,
+                    "message": "たぶんタチツボスミレじゃないかな？",
+                },
+                {
+                    "speciesName": "オオタチツボスミレ",
+                    "respondentName": "ミノリ",
+                    "respondentImageURL": "https://example.com/charactors/minori/image.jpg",
+                    "score": score,
+                    "message": "ひょっとしたらオオタチツボスミレかもしれない。",
+                },
+            ]
+        }
+    elif name == "Fish":
+        return {
+            "answers": [
+                {
+                    "speciesName": "カタクチイワシ",
+                    "respondentName": "トト",
+                    "respondentImageURL": "https://example.com/charactors/toto/image.jpg",
+                    "score": score,
+                    "message": "これはカタクチイワシだよ。",
+                },
+                {
+                    "speciesName": "トウゴロウイワシ",
+                    "respondentName": "トト",
+                    "respondentImageURL": "https://example.com/charactors/toto/image.jpg",
+                    "score": score,
+                    "message": "これはトウゴロウイワシかもしれないねー。",
+                },
+            ]
+        }
+    return {"error": "Unknown target"}
