fe800d94cea18b6d8d173cb5d197073728b2aecf
Kondo Daisuke <gim0kondo@gmail.com>
2021-02-19 01:11:56 +0900

    :sparkles: detect text on image by Vision API

--- brain/app/gcp/vision.py
+++ brain/app/gcp/vision.py
@@ -1,5 +1,5 @@
 from functools import reduce
-from typing import Any, AnyStr, Dict
+from typing import Any, AnyStr, Dict, List
 
 from google.cloud import vision
 
@@ -32,3 +32,47 @@ def label_image(image: AnyStr) -> Dict[str, Any]:
         "name": best_label.description,
         "score": best_label.score,
     }
+
+
+def ocr_image(image: AnyStr) -> List[Dict[str, Any]]:
+    """OCR image by Vision API
+
+    Args:
+        image (AnyStr): image data to label
+
+    Raises:
+        Exception: Something happens on calling Vision API
+
+    Returns:
+        List[Dict[str, Any]]: detected texts info
+    """
+    client = vision.ImageAnnotatorClient()
+
+    vision_image = vision.Image(content=image)
+
+    response = client.text_detection(image=vision_image)
+    # print(response)
+    if response.error.message:
+        raise Exception(response.error.message)
+
+    if len(response.text_annotations) < 2:
+        return []
+
+    texts = []
+    for annotation in response.text_annotations[1:]:
+        vertices = annotation.bounding_poly.vertices
+        print("----------------------")
+        print(annotation.description)
+        print(annotation.bounding_poly)
+        print("----------------------")
+        text_data = {
+            "text": annotation.description,
+            "vertices": {
+                "left_upper": {"x": vertices[0].x, "y": vertices[0].y},
+                "right_upper": {"x": vertices[1].x, "y": vertices[1].y},
+                "right_bottom": {"x": vertices[2].x, "y": vertices[2].y},
+                "left_bottom": {"x": vertices[3].x, "y": vertices[3].y},
+            },
+        }
+        texts.append(text_data)
+    return texts
--- brain/tests/gcp/test_vision.py
+++ brain/tests/gcp/test_vision.py
@@ -1,7 +1,7 @@
 import io
 import pathlib
 
-from app.gcp.vision import label_image
+from app.gcp.vision import label_image, ocr_image
 
 
 def test_label_image():
@@ -12,3 +12,14 @@ def test_label_image():
         image = image_file.read()
     label = label_image(image)
     assert label["name"] == "Plant"
+
+
+def test_ocr_image():
+    this_dir = pathlib.Path(__file__).parent
+    sample_image_path = f"{this_dir}/../../samples/note.jpg"
+
+    with io.open(sample_image_path, "rb") as image_file:
+        image = image_file.read()
+    texts = ocr_image(image)
+    assert len(texts) > 3
+    assert texts[0]["text"] == "This"
