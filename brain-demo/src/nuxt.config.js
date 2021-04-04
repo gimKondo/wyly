259d8373f2545acba90544960acb0f1d7dea5515
Kondo Daisuke <gim0kondo@gmail.com>
2021-04-04 23:47:39 +0900

    :green_heart: set up lint for typescript on nuxt

--- brain-demo/src/nuxt.config.js
+++ brain-demo/src/nuxt.config.js
@@ -2,5 +2,12 @@ export default {
   generate: {
     dir: '../public'
   },
-  buildModules: ['@nuxt/typescript-build']
+  buildModules: ['@nuxt/typescript-build'],
+  typescript: {
+    typeCheck: {
+      eslint: {
+        files: './**/*.{ts,js,vue}'
+      }
+    }
+  }
 }
