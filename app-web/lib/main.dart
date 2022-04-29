a657904f5c8cc03ba841383d1c5ffe98d66ba028
Kondo Daisuke <gim0kondo@gmail.com>
2022-04-29 23:31:28 +0900

    :sparkles: check required field

--- app-web/lib/main.dart
+++ app-web/lib/main.dart
@@ -63,11 +63,12 @@ class _SignUpFormState extends State<SignUpForm> {
   final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
 
-  final double _formProgress = 0;
+  double _formProgress = 0;
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -109,7 +110,7 @@ class _SignUpFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: const Text('Sign up'),
           ),
         ],
@@ -119,5 +120,24 @@ class _SignUpFormState extends State<SignUpForm> {
 
   void _showWelcomeScreen() {
     Navigator.of(context).pushNamed('/welcome');
+  }
+
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
   }
 }
